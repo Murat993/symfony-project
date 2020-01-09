1b992ca9c62874354408ca6f9624dcbebe579456
Murat <muratnysambaev93@gmail.com>
2020-01-09 14:18:44 +0600

    добавление network auth

--- manager/src/Model/User/Entity/User/User.php
+++ manager/src/Model/User/Entity/User/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\User\Entity\User;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -10,6 +11,7 @@ class User
 {
     private const STATUS_WAIT = 'wait';
     private const STATUS_ACTIVE = 'active';
+    private const STATUS_NEW = 'new';
     /**
      * @var Id
      */
@@ -19,11 +21,11 @@ class User
      */
     private $date;
     /**
-     * @var Email
+     * @var Email|null
      */
     private $email;
     /**
-     * @var string
+     * @var string|null
      */
     private $passwordHash;
     /**
@@ -34,25 +36,51 @@ class User
      * @var string
      */
     private $status;
+    /**
+     * @var Network[]|ArrayCollection
+     */
+    private $networks;
 
-    public function __construct(Id $id, \DateTimeImmutable $date, Email $email, string $hash, string $token)
+    public function __construct(Id $id, \DateTimeImmutable $date)
     {
         $this->id = $id;
         $this->date = $date;
+        $this->status = self::STATUS_NEW;
+        $this->networks = new ArrayCollection();
+    }
+
+    public function signUpByEmail(Email $email, string $hash, string $token): void
+    {
+        if (!$this->isNew()) {
+            throw new \DomainException('User is already signed up.');
+        }
         $this->email = $email;
         $this->passwordHash = $hash;
         $this->confirmToken = $token;
-        $this->status = self::STATUS_WAIT;
     }
 
-    public function confirmSignUp(): void
+    public function signUpByNetwork(string $network, string $identity): void
     {
-        if (!$this->isWait()) {
-            throw new \DomainException('User is already confirmed.');
+        if (!$this->isNew()) {
+            throw new \DomainException('User is already signed up.');
         }
-
+        $this->attachNetwork($network, $identity);
         $this->status = self::STATUS_ACTIVE;
-        $this->confirmToken = null;
+    }
+
+    private function attachNetwork(string $network, string $identity): void
+    {
+        foreach ($this->networks as $existing) {
+            if ($existing->isForNetwork($network)) {
+                throw new \DomainException('Network is already attached.');
+            }
+        }
+        $this->networks->add(new Network($this, $network, $identity));
+    }
+
+    public function isNew(): bool
+    {
+        return $this->status === self::STATUS_NEW;
     }
 
     public function isWait(): bool
@@ -75,12 +103,12 @@ class User
         return $this->date;
     }
 
-    public function getEmail(): Email
+    public function getEmail(): ?Email
     {
         return $this->email;
     }
 
-    public function getPasswordHash(): string
+    public function getPasswordHash(): ?string
     {
         return $this->passwordHash;
     }
@@ -88,5 +116,13 @@ class User
     public function getConfirmToken(): ?string
     {
         return $this->confirmToken;
+    }
+
+    /**
+     * @return Network[]
+     */
+    public function getNetworks(): array
+    {
+        return $this->networks->toArray();
     }
 }
