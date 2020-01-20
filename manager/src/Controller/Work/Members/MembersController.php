6a85fe054b177db002583c2ed3bfbbde89540d9b
Murat <muratnysambaev93@gmail.com>
2020-01-20 11:45:47 +0600

    кастомная анатация (регулярка), чтобы не сбилист пути между собой

--- manager/src/Controller/Work/Members/MembersController.php
+++ manager/src/Controller/Work/Members/MembersController.php
@@ -20,9 +20,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Annotation\Guid;
 
 /**
- * @Route("/work/members", name="work.members")
+ * @Route("/work/members", name="work.members", requirements={Guid::PATTERN})
  * @IsGranted("ROLE_WORK_MANAGE_MEMBERS")
  */
 class MembersController extends AbstractController
