822e464cd21fb51501abb4723775b5082bd29edf
Murat <muratnysambaev93@gmail.com>
2020-01-22 15:12:12 +0600

    Update Symfony

--- manager/src/Model/User/Service/PasswordHasher.php
+++ manager/src/Model/User/Service/PasswordHasher.php
@@ -11,7 +11,7 @@ class PasswordHasher
 {
     public function hash(string $password): string
     {
-        $hash = password_hash($password, PASSWORD_ARGON2I);
+        $hash = password_hash($password, PASSWORD_BCRYPT);
         if ($hash === false) {
             throw new RuntimeException('Unable to generate hash.');
         }
