ffe00636c1ec424cdeb706c3592f78d9f18e41e3
Murat <muratnysambaev93@gmail.com>
2020-01-23 10:52:11 +0600

    создания и редактирования задачи

---
+++ manager/src/Model/Work/Entity/Projects/Task/TaskRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\Entity\Projects\Task;
+
+use App\Model\EntityNotFoundException;
+use Doctrine\ORM\EntityManagerInterface;
+
+class TaskRepository
+{
+    private $em;
+    private $connection;
+    private $repo;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->connection = $em->getConnection();
+        $this->repo = $em->getRepository(Task::class);
+    }
+
+    public function get(Id $id): Task
+    {
+        /** @var Task $task */
+        if (!$task = $this->repo->find($id->getValue())) {
+            throw new EntityNotFoundException('Task is not found.');
+        }
+        return $task;
+    }
+
+    public function add(Task $task): void
+    {
+        $this->em->persist($task);
+    }
+
+    public function nextId(): Id
+    {
+        return new Id((int)$this->connection->query('SELECT nextval(\'work_projects_tasks_seq\')')->fetchColumn());
+    }
+}
