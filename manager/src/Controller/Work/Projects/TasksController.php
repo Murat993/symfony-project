3d12ca17444d6618fdc04ed692f43182ad6d96e7
Murat <muratnysambaev93@gmail.com>
2020-01-28 22:26:12 +0600

    вывод историй задач в список (в табе)

--- manager/src/Controller/Work/Projects/TasksController.php
+++ manager/src/Controller/Work/Projects/TasksController.php
@@ -33,7 +33,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\ReadModel\Work\Projects\ActionFetcher;
+use App\ReadModel\Work\Projects\Action\ActionFetcher;
 
 /**
  * @Route("/work/projects/tasks", name="work.projects.tasks")
