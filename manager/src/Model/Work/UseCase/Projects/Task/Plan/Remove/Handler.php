cf365760988a54c9ec811d9f68339e3a8a2e4e23
Murat <muratnysambaev93@gmail.com>
2020-02-02 14:45:03 +0600

    добавление в TaskController API метовды plan, planremove

--- manager/src/Model/Work/UseCase/Projects/Task/Plan/Remove/Handler.php
+++ manager/src/Model/Work/UseCase/Projects/Task/Plan/Remove/Handler.php
@@ -5,15 +5,16 @@ declare(strict_types=1);
 namespace App\Model\Work\UseCase\Projects\Task\Plan\Remove;
 
 use App\Model\Flusher;
-use App\Model\Work\Entity\Projects\Task\Id;
-use App\Model\Work\Entity\Projects\Task\TaskRepository;
 use App\Model\Work\Entity\Members\Member\Id as MemberId;
 use App\Model\Work\Entity\Members\Member\MemberRepository;
+use App\Model\Work\Entity\Projects\Task\Id;
+use App\Model\Work\Entity\Projects\Task\TaskRepository;
 
 class Handler
 {
     private $tasks;
     private $flusher;
+    private $members;
 
     public function __construct(
         TaskRepository $tasks,
@@ -31,7 +32,7 @@ class Handler
         $actor = $this->members->get(new MemberId($command->actor));
         $task = $this->tasks->get(new Id($command->id));
 
-        $task->plan($actor, new \DateTimeImmutable(), null);
+        $task->removePlan($actor, new \DateTimeImmutable());
 
         $this->flusher->flush($task);
     }
--- manager/tests/Unit/Model/Work/Entity/Projects/Task/ChangeProgressTest.php
+++ manager/tests/Unit/Model/Work/Entity/Projects/Task/ChangeProgressTest.php
@@ -11,7 +11,7 @@ use App\Tests\Builder\Work\Projects\ProjectBuilder;
 use App\Tests\Builder\Work\Projects\TaskBuilder;
 use PHPUnit\Framework\TestCase;
 
-class ChangeStatusTest extends TestCase
+class ChangeProgressTest extends TestCase
 {
     public function testSuccess(): void
     {
