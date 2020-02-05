c4755b95c3744d962e4ec25158bbafa2b2938b96
Murat <muratnysambaev93@gmail.com>
2020-02-05 20:36:39 +0600

    добавление проверки доступа чере аннотацию

--- manager/src/Controller/HomeController.php
+++ manager/src/Controller/HomeController.php
@@ -8,7 +8,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Annotation\RequiresUserCredits;
 
+/**
+ * @RequiresUserCredits()
+ */
 class HomeController extends AbstractController
 {
     /**
